90ae3a3de0ecdc36443ebc70f0265e6875326f72
Daniel <benjamindaniel706@gmail.com>
2024-12-20 14:28:23 +0100

    chore: init tanstack router

---
+++ frontend/src/config/env.ts
@@ -0,0 +1,3 @@
+export const __DEV__ = import.meta.env.DEV;
+
+export const BASE_API_ROUTE = import.meta.env.VITE_BASE_API_ROUTE;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from "@tanstack/react-router";
+import { StrictMode } from "react";
+import ReactDOM from "react-dom/client";
+import { queryClient } from "./config/tanstack-query";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// Import the generated route tree
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "sonner";
+import "./index.css";
+import { router } from "./router";
+
+// Render the app
+const rootElement = document.getElementById("root")!;
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </StrictMode>
+  );
+}
